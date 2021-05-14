5a7a76b982eba730a40b525750fe3229ce451fa5
umit-turk <uyt_turk@hotmail.com>
2021-05-14 13:58:58 +0300

    created template engine

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 //request-response döngüsü içerisinde kalan herşeye middleware denir.
 
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs'); //
+
 //MIDDLEWEARS
 app.use(express.static('public'));
 
+//ROUTES
 app.get('/', (req, res) => {
-  //dirname benim proje klosörümün olduğu yer
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
 });
+app.get('/about', (req, res) => {
+  res.render('about'); 
+});
+app.get('/add', (req, res) => {
+  res.render('add'); 
+});
+
 
 const port = 3000;
 app.listen(port, () => {
