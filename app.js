4d777c3d2df051c2000b417ea75e837beb011ace
umit-turk <uyt_turk@hotmail.com>
2021-05-14 12:45:21 +0300

    Static files upload

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const fn = function () {
-  console.log('function working');
-};
+//request-response döngüsü içerisinde kalan herşeye middleware denir.
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//MIDDLEWEARS
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  //dirname benim proje klosörümün olduğu yer
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı `);
+});
