e1efdc364331442716d8ea82b015f81317c0b3f6
umit-turk <uyt_turk@hotmail.com>
2021-05-17 15:10:39 +0300

    model ve dinamik

--- app.js
+++ app.js
@@ -1,28 +1,47 @@
 //request-response döngüsü içerisinde kalan herşeye middleware denir.
 
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
+
 
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db'), {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs'); //
 
 //MIDDLEWEARS
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));//body bilgisini yakalamak için 2 adet middleware fonksiyonunu kullanmamız gerekir.
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({})//veritabanımdaki fotoğraflarımızı gösterecek
+  res.render('index', {
+    photos
+  });
 });
 app.get('/about', (req, res) => {
-  res.render('about'); 
+  res.render('about');
 });
 app.get('/add', (req, res) => {
-  res.render('add'); 
+  res.render('add');
 });
+app.post('/photos', async (req, res) => {
+  //Burada "add photo" sayfadan gelen post metodunu yakalamamız için ilgili yönlendirmeyi yakalayıp, consolda request nesnesinden gelen body bilgisini yakalayalım.
 
+  await Photo.create(req.body)//photomuzu req.body den gelen veriyi kullanacağız.
+  res.redirect('/')
+});
 
 const port = 3000;
 app.listen(port, () => {
