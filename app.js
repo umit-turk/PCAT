3d5744208c3823b84c075195e1dfea8415a464a2
umit-turk <uyt_turk@hotmail.com>
2021-05-21 20:38:49 +0300

    MVC

--- app.js
+++ app.js
@@ -3,11 +3,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
-const path = require('path');
-const Photo = require('./models/Photo');
 const fileUpload = require('express-fileupload'); // modülü kullanıma alıyoruz.
-const fs = require('fs');
 const methodOverride = require('method-override');
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageControllers');
 const app = express();
 
 //connect DB
@@ -33,73 +32,17 @@ app.use(
 );
 
 //ROUTES
-app.get('/', async (req, res) => {
-  const photos = await Photo.find({}).sort('-dataCreated'); //veritabanımdaki fotoğraflarımızı gösterecek
-  res.render('index', {
-    photos,
-  });
-});
-app.get('/about', async (req, res) => {
-  res.render('about');
-});
-app.get('/photos/:id', async (req, res) => {
-  //res.render('about');
-  const photo = await Photo.findById(req.params.id); //modelimizde id yardımıyla hangi fotoğraf olduğunu buluyoruz
-  res.render('photo', {
-    photo,
-  });
-});
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
 
-app.get('/add', (req, res) => {
-  res.render('add');
-});
-app.post('/photos', async (req, res) => {
-  //Burada "add photo" sayfadan gelen post metodunu yakalamamız için ilgili yönlendirmeyi yakalayıp, consolda request nesnesinden gelen body bilgisini yakalayalım.
 
-  //await Photo.create(req.body)//photomuzu req.body den gelen veriyi kullanacağız.
-  // res.redirect('/')
-  const uploadDir = 'public/uploads';
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
 
-  if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-  }
-
-  let uploadeImage = req.files.image;
-  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
-
-  uploadeImage.mv(uploadPath, async () => {
-    await Photo.create({
-      ...req.body,
-      image: '/uploads/' + uploadeImage.name,
-    });
-    res.redirect('/');
-  });
-});
-
-app.get('/photos/edit/:id', async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  res.render('edit', {
-    photo,
-  });
-});
-
-app.put('/photos/:id', async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  photo.title = req.body.title;
-  photo.description = req.body.description;
-  photo.save();
-
-  res.redirect(`/photos/${req.params.id}`);
- 
-});
-
-app.delete('/photos/:id', async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  let deletedImage = __dirname + '/public' + photo.image;
-  fs.unlinkSync(deletedImage); // veri tabanındaki bilgileri silmesini istiyoruz.
-  await Photo.findByIdAndRemove(req.params.id);
-  res.redirect('/');
-});
 
 const port = 3000;
 app.listen(port, () => {
